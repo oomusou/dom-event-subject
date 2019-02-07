333b8e85778125d497f79b07d89914f085d244d7
oomusou <oomusou@gmail.com>
2019-02-07 11:26:27 +0800

    DOM Event Subject

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,9 +1,28 @@
 <template>
-  <div></div>
+  <div>
+    <button v-stream:click="{ subject: click$, data: 2 }">+</button>
+    <h1>{{ random$ }}</h1>
+  </div>
 </template>
 
 <script>
+import { map, startWith, scan, pluck } from 'rxjs/operators';
+
+const subscriptions = function() {
+  const random$ = this.click$.pipe(
+    pluck('data'),
+    startWith(0),
+    scan((a, x) => a + x),
+  );
+
+  return {
+    random$
+  };
+};
+
 export default {
-  name: 'HelloWorld'
+  name: 'HelloWorld',
+  domStreams: ['click$'],
+  subscriptions,
 };
 </script>
